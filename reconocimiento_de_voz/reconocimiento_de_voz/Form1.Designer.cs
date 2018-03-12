c8b28205b728b2a34bc34fa9426f2984361a91a8
josemanuelreviriego <josemanuel.reviriego.ciclos@gmail.com>
2018-03-12 21:02:39 +0100

    Pequeña prueba de reconocimiento de voz

---
+++ reconocimiento_de_voz/reconocimiento_de_voz/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace reconocimiento_de_voz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.negro = new System.Windows.Forms.Button();
+            this.azul = new System.Windows.Forms.Button();
+            this.rojo = new System.Windows.Forms.Button();
+            this.verde = new System.Windows.Forms.Button();
+            this.amarillo = new System.Windows.Forms.Button();
+            this.blanco = new System.Windows.Forms.Button();
+            this.marron = new System.Windows.Forms.Button();
+            this.rosa = new System.Windows.Forms.Button();
+            this.morado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // negro
+            // 
+            this.negro.BackColor = System.Drawing.Color.Black;
+            this.negro.Location = new System.Drawing.Point(13, 13);
+            this.negro.Name = "negro";
+            this.negro.Size = new System.Drawing.Size(125, 99);
+            this.negro.TabIndex = 0;
+            this.negro.Text = "button1";
+            this.negro.UseVisualStyleBackColor = false;
+            // 
+            // azul
+            // 
+            this.azul.BackColor = System.Drawing.Color.Blue;
+            this.azul.Location = new System.Drawing.Point(144, 12);
+            this.azul.Name = "azul";
+            this.azul.Size = new System.Drawing.Size(125, 99);
+            this.azul.TabIndex = 1;
+            this.azul.Text = "button2";
+            this.azul.UseVisualStyleBackColor = false;
+            // 
+            // rojo
+            // 
+            this.rojo.BackColor = System.Drawing.Color.Red;
+            this.rojo.Location = new System.Drawing.Point(275, 13);
+            this.rojo.Name = "rojo";
+            this.rojo.Size = new System.Drawing.Size(125, 99);
+            this.rojo.TabIndex = 2;
+            this.rojo.Text = "button3";
+            this.rojo.UseVisualStyleBackColor = false;
+            // 
+            // verde
+            // 
+            this.verde.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.verde.Location = new System.Drawing.Point(13, 117);
+            this.verde.Name = "verde";
+            this.verde.Size = new System.Drawing.Size(125, 99);
+            this.verde.TabIndex = 3;
+            this.verde.Text = "button4";
+            this.verde.UseVisualStyleBackColor = false;
+            // 
+            // amarillo
+            // 
+            this.amarillo.BackColor = System.Drawing.Color.Yellow;
+            this.amarillo.Location = new System.Drawing.Point(144, 117);
+            this.amarillo.Name = "amarillo";
+            this.amarillo.Size = new System.Drawing.Size(125, 99);
+            this.amarillo.TabIndex = 4;
+            this.amarillo.Text = "button5";
+            this.amarillo.UseVisualStyleBackColor = false;
+            // 
+            // blanco
+            // 
+            this.blanco.BackColor = System.Drawing.Color.White;
+            this.blanco.Location = new System.Drawing.Point(275, 117);
+            this.blanco.Name = "blanco";
+            this.blanco.Size = new System.Drawing.Size(125, 99);
+            this.blanco.TabIndex = 5;
+            this.blanco.Text = "button6";
+            this.blanco.UseVisualStyleBackColor = false;
+            // 
+            // marron
+            // 
+            this.marron.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.marron.Location = new System.Drawing.Point(13, 222);
+            this.marron.Name = "marron";
+            this.marron.Size = new System.Drawing.Size(125, 99);
+            this.marron.TabIndex = 6;
+            this.marron.Text = "button7";
+            this.marron.UseVisualStyleBackColor = false;
+            // 
+            // rosa
+            // 
+            this.rosa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.rosa.Location = new System.Drawing.Point(144, 222);
+            this.rosa.Name = "rosa";
+            this.rosa.Size = new System.Drawing.Size(125, 99);
+            this.rosa.TabIndex = 7;
+            this.rosa.Text = "button8";
+            this.rosa.UseVisualStyleBackColor = false;
+            // 
+            // morado
+            // 
+            this.morado.BackColor = System.Drawing.Color.Purple;
+            this.morado.Location = new System.Drawing.Point(275, 222);
+            this.morado.Name = "morado";
+            this.morado.Size = new System.Drawing.Size(125, 99);
+            this.morado.TabIndex = 8;
+            this.morado.Text = "button9";
+            this.morado.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 350);
+            this.Controls.Add(this.morado);
+            this.Controls.Add(this.rosa);
+            this.Controls.Add(this.marron);
+            this.Controls.Add(this.blanco);
+            this.Controls.Add(this.amarillo);
+            this.Controls.Add(this.verde);
+            this.Controls.Add(this.rojo);
+            this.Controls.Add(this.azul);
+            this.Controls.Add(this.negro);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button negro;
+        private System.Windows.Forms.Button azul;
+        private System.Windows.Forms.Button rojo;
+        private System.Windows.Forms.Button verde;
+        private System.Windows.Forms.Button amarillo;
+        private System.Windows.Forms.Button blanco;
+        private System.Windows.Forms.Button marron;
+        private System.Windows.Forms.Button rosa;
+        private System.Windows.Forms.Button morado;
+    }
+}
+
---
+++ reconocimiento_de_voz/reconocimiento_de_voz/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace reconocimiento_de_voz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        void reconocimiento(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Text == "negro")
+            {
+                negro.Visible = true;
+            }
+            else if (e.Result.Text == "azul")
+            {
+                azul.Visible = true;
+            }
+            else if (e.Result.Text == "rojo")
+            {
+                rojo.Visible = true;
+            }
+            else if (e.Result.Text == "verde")
+            {
+                verde.Visible = true;
+            }
+            else if (e.Result.Text == "amarillo")
+            {
+                amarillo.Visible = true;
+            }
+            else if (e.Result.Text == "blanco")
+            {
+                blanco.Visible = true;
+            }
+            else if (e.Result.Text == "marron")
+            {
+                marron.Visible = true;
+            }
+            else if (e.Result.Text == "rosa")
+            {
+                rosa.Visible = true;
+            }
+            else if (e.Result.Text == "morado")
+            {
+                morado.Visible = true;
+            }
+            else if (e.Result.Text == "todos")
+            {
+                negro.Visible = true;
+                azul.Visible = true;
+                rojo.Visible = true;
+                verde.Visible = true;
+                amarillo.Visible = true;
+                blanco.Visible = true;
+                marron.Visible = true;
+                rosa.Visible = true;
+                morado.Visible = true;
+
+            }
+            else if (e.Result.Text == "ninguno")
+            {
+                negro.Visible = false;
+                azul.Visible = false;
+                rojo.Visible = false;
+                verde.Visible = false;
+                amarillo.Visible = false;
+                blanco.Visible = false;
+                marron.Visible = false;
+                rosa.Visible = false;
+                morado.Visible = false;
+            }
+            else if(e.Result.Text == "Salir")
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            negro.Visible = false;
+            azul.Visible = false;
+            rojo.Visible = false;
+            verde.Visible = false;
+            amarillo.Visible = false;
+            blanco.Visible = false;
+            marron.Visible = false;
+            rosa.Visible = false;
+            morado.Visible = false;
+
+            Choices lista = new Choices();
+            SpeechRecognitionEngine grabar = new SpeechRecognitionEngine();
+
+            lista.Add(new string[] {"negro", "azul", "rojo", "verde", "amarillo", "blanco",
+        "marron", "rosado", "morado", "todos", "ninguno", "salir"});
+
+            Grammar gramatica = new Grammar(new GrammarBuilder(lista));
+
+            try
+            {
+                grabar.SetInputToDefaultAudioDevice();
+                grabar.LoadGrammar(gramatica);
+                grabar.SpeechRecognized += reconocimiento;
+                grabar.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+
+
+        }
+    }
+}
+
